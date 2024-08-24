0c4d3f3c2cfdf077185eb4d88670c3f5e56b87b3
Fedor <fedor.kochkin48@gmail.com>
2024-08-24 23:36:11 +0300

    Добавьте файлы проекта.

---
+++ Hw14.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+int main()
+{
+	std::string Word = "Delay";
+	std::cout << "String = " << Word << "\n";
+	std::cout << "String length = " << Word.length() << "\n";
+	std::cout << "first symbol = " << Word[0] << "\n";
+	std::cout << "last symbol = " << Word[Word.length() - 1];
+}
+
